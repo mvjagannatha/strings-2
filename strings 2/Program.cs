602977aef67b82f42fb1121138704c0dd0b10cb2
Admin <Admin@DESKTOP-6PQE64G>
2024-01-14 20:24:27 +0530

    Add project files.

---
+++ strings 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Increment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int i = 0;
+            Console.Write("Enter a string:");
+            str = Console.ReadLine();
+            char[] nstr = new char[str.Length]; //Convert a string into character array.
+            while (i < str.Length)
+            {
+                if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
+                {
+                    nstr[i] = str[i];
+                }
+                nstr[i]++;
+                i++;
+            }
+            
+            Console.Write(nstr);
+            Console.ReadKey();
+        }
+    }
+}
